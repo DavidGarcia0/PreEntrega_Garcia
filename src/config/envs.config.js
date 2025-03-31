3b231b8b2bac2f9c66f808d01db0d13d348ae9aa
DavidGarcia0 <davidgarciaf0404@gmail.com>
2025-03-31 19:17:17 -0300

    Update envs.config.js

--- src/config/envs.config.js
+++ src/config/envs.config.js
@@ -1,7 +1,7 @@
 
 export default {
   PORT: process.env.PORT || 4000,
-  MONGO_URL: process.env.MONGO_URL || 'mongodb+srv://davidgarciaf0404:0T3WIaUO33fO0vHR@cluster0.kojjgtt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  MONGO_URL: process.env.MONGO_URL,
   SESSION_SECRET: process.env.SESSION_SECRET || "secret",
   JWT_SECRET: process.env.JWT_SECRET || "secret"
 }
